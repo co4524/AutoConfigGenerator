0335d2e2db728d341c64e5f4428cc1eccbaf90ff
co4524 <monster3697@gmail.com>
2019-06-28 04:48:06 +0800

    first commit

--- remoteConfig/changeIp.sh
+++ remoteConfig/changeIp.sh
@@ -1,9 +1,11 @@
 path=$HOME/mytestnet/node
-argc=$#
-argv=($@)
+nodeNum=$1
+base=$2
 for ((i=0 ; i<$1 ; i++)) {
+	ip=$3
 	for ((j=0 ; j<$1 ; j++)){
-		ip=${argv[j+1]}
-		sed -i "s/node$j/$ip/g" $path$i/tendermint/config/config.toml	
+		#echo $base.$ip
+		sed -i "s/node$j/$base.$ip/g" $path$i/tendermint/config/config.toml
+		let ip=ip+1	
 	}
 }
